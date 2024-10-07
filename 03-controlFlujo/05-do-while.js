54d2242f2ae77912ded9215cdeedf63d1776326e
Alejandro Muñoz <alejo.m.h@outlook.com>
2024-10-07 17:02:47 -0500

    do while

--- 03-controlFlujo/05-do-while.js
+++ 03-controlFlujo/05-do-while.js
@@ -2,8 +2,23 @@
 
 let i = 0;
 
-while (i < 10) {
-    console.log(i);
-    i++; 
+// while (i < 3) {
+//     if (i % 2 == 0) {
+//         console.log('Número par', i);
+//     }
+    
+//     i++;
+// }  
 
-}
+//    --    While lee primero la condición y luego ejcuta
+
+do {
+    if (i % 2 == 0) {
+        console.log('Número par', i);
+    }
+        
+    i++;
+} while (i > 5); 
+
+
+//    --   DO while Ejecuta y lego mira la condición 
