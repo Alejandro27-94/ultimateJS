668e458e82445e7655a91a9212e44da194eea572
Alejandro Muñoz <alejo.m.h@outlook.com>
2025-01-03 19:56:33 -0500

    :

---
+++ 03-controlFlujo/06-for.js
@@ -0,0 +1,23 @@
+// for al igual que while es un loop pero a diferencia se compone de tres expresiones
+// 1 inicializacion de variable 
+// 2 comparacion de i 
+// 3 ejecucion de accion 
+
+/*
+let i = 0;
+
+while (i < 10) {
+    if (i % 2 == 0){
+        console.log('Número par', i);
+    }
+    i++;
+}
+*/
+
+
+for (i = 2;i < 10;i++) {
+    if (i % 2 == 0){
+        console.log('Número par', i);
+    }
+}
+
